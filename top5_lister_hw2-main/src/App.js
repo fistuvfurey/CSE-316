1e76a1e9c0407d4aa2b532fd9229233251c403b9
Aidan Furey <aidan.t.furey@gmail.com>
2021-10-04 00:49:48 -0400

    Newly created lists are now saved in local storage.

--- top5_lister_hw2-main/src/App.js
+++ top5_lister_hw2-main/src/App.js
@@ -76,6 +76,7 @@ class App extends React.Component {
             // PUTTING THIS NEW LIST IN PERMANENT STORAGE
             // IS AN AFTER EFFECT
             this.db.mutationCreateList(newList);
+            this.db.mutationUpdateSessionData(this.state.sessionData);
         });
     }
     renameList = (key, newName) => {
